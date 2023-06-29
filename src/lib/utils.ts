260276093d06140a45e77801dd828614eaba7738
ebubekiryzc <ebubekiryazi@gmail.com>
2023-06-29 14:14:02 +0300

    added new utils for localStorage methods

---
+++ src/lib/utils.ts
@@ -0,0 +1,17 @@
+export function setLocalStorageItem(key: string, value: any) {
+  try {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    // catch possible errors:
+    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+  }
+}
+
+export function getLocalStorageItem(key: string, initialValue: any) {
+  try {
+    const value = window.localStorage.getItem(key);
+    return value ? JSON.parse(value) : initialValue;
+  } catch (e) {
+    return initialValue;
+  }
+}
