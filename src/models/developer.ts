f2a184dfffd4c2c0e70f695d48a1647ca93356dd
ebubekiryzc <ebubekiryazi@gmail.com>
2023-06-30 00:19:44 +0300

    added new model

---
+++ src/models/developer.ts
@@ -0,0 +1,14 @@
+export interface Developer {
+  id: number;
+  slug: string;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  biography: string;
+  contact_number: string;
+  address: string;
+  profile_photo: string;
+  created_at: Date;
+  updated_at: Date;
+}
