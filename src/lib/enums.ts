1e2f826a07e200a47f3add1cf4308368cecdb837
ebubekiryzc <ebubekiryazi@gmail.com>
2023-07-03 15:21:11 +0300

    created new enum types for storing constant values

---
+++ src/lib/enums.ts
@@ -0,0 +1,15 @@
+export enum AnimationType {
+  SPIN = "spinEffect",
+  BOUNCE = "bounceEffect",
+  PULSE_AND_SCALE = "pulseAndScaleEffect",
+}
+
+export enum Colors {
+  PRIMARY_COLOR = "var(--primary-color)",
+  ORANGE_COLOR = "var(--orange-color)",
+  GREEN_COLOR = "var(--green-color)",
+  RED_COLOR = "var(--red-color)",
+  DARK_BLUE_COLOR = "var(--dark-blue-color)",
+  BACKGROUND_COLOR = "var(--background-color)",
+  OPPOSITE_BACKGROUND_COLOR = "var(--opposite-background-color)",
+}
